3cdd2eb02be95618268b0e459de934b9f60dffeb
Sindre Sorhus <sindresorhus@gmail.com>
2016-08-04 16:00:49 +0200

    do loose parsing to allow almost valid semver to parse
    I absolutely hate this, but I have to be realistic...
    
    fixes #2

--- test.js
+++ test.js
@@ -6,4 +6,5 @@ test(t => {
 	t.is(fn('1.2.3+foo', 'patch'), '1.2.3');
 	t.is(fn('1.2.3', 'minor'), '1.2.0');
 	t.is(fn('1.2.3', 'major'), '1.0.0');
+	t.is(fn('7.0.8-0ubuntu0.16.04.2', 'patch'), '7.0.8', 'PHP on Ubuntu');
 });
