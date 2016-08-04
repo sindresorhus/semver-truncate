0d1165c0db1f3b6041024b805756c96f04a11c92
Sindre Sorhus <sindresorhus@gmail.com>
2016-08-04 16:05:10 +0200

    meta tweaks

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import test from 'ava';
-import fn from './';
+import m from './';
 
 test(t => {
-	t.is(fn('1.2.3-foo', 'patch'), '1.2.3');
-	t.is(fn('1.2.3+foo', 'patch'), '1.2.3');
-	t.is(fn('1.2.3', 'minor'), '1.2.0');
-	t.is(fn('1.2.3', 'major'), '1.0.0');
-	t.is(fn('7.0.8-0ubuntu0.16.04.2', 'patch'), '7.0.8', 'PHP on Ubuntu');
+	t.is(m('1.2.3-foo', 'patch'), '1.2.3');
+	t.is(m('1.2.3+foo', 'patch'), '1.2.3');
+	t.is(m('1.2.3', 'minor'), '1.2.0');
+	t.is(m('1.2.3', 'major'), '1.0.0');
+	t.is(m('7.0.8-0ubuntu0.16.04.2', 'patch'), '7.0.8', 'PHP on Ubuntu');
 });
